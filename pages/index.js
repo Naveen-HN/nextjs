22d9fc3a11de5dff9c83b3a933fdfa15901324c5
Naveen-HN <hnnaveen93@gmail.com>
2021-02-06 15:45:08 -0500

    initial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <meta name="keywords" content="web developement, nextjs, programming" />
+      </Head>
+      <h1>Welcome to NextJS</h1>
+    </div>
+  );
+}
